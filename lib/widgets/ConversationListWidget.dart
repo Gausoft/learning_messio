2775d026e0d02992718e734fbdb5b4f3241c85e7
Gauthier Eholoum <thiergau691@gmail.com>
2019-11-13 06:04:16 +0000

    feat: App icon added

---
+++ lib/widgets/ConversationListWidget.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'ChatRowWidget.dart';
+
+class ConversationListWidget extends StatefulWidget {
+  @override
+  State createState() => _ConversationListWidgetState();
+}
+
+class _ConversationListWidgetState extends State<ConversationListWidget> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+  }
+}
